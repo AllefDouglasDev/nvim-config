b190f0f1cb2f6c77cba1b96209462b09fcd7f7ec
Allef Douglas <allefwalker73@gmail.com>
2023-09-11 18:19:38 -0300

    improve nullls to ignore when does not have a eslint configuration file

--- lua/allef/plugins/nullls.lua
+++ lua/allef/plugins/nullls.lua
@@ -19,14 +19,30 @@ return {
             }
         }
 
-        -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+        local function contains_any_ignore_case(inputString, stringsToCompare)
+            for _, searchString in ipairs(stringsToCompare) do
+                local pattern = string.gsub(searchString, "[%a]", function(c)
+                    return "[" .. string.lower(c) .. string.upper(c) .. "]"
+                end)
+                if string.find(inputString, pattern) then
+                    return true
+                end
+            end
+            return false
+        end
+
         local formatting = null_ls.builtins.formatting
         null_ls.setup({
             debug = false,
             sources = {
                 formatting.prettier,
                 null_ls.builtins.code_actions.eslint_d,
-                null_ls.builtins.diagnostics.eslint_d,
+                null_ls.builtins.diagnostics.eslint_d.with({
+                    filter = function(diagnostic)
+                        local ignore = { "no eslint configuration found" }
+                        return not contains_any_ignore_case(diagnostic.message, ignore)
+                    end,
+                }),
             },
         })
 
