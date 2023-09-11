ab1998edc374dae8bb60871295c9a259b0f0a850
Allef Douglas <allefwalker73@gmail.com>
2023-09-11 15:11:12 -0300

    removing commented imports

--- init.lua
+++ init.lua
@@ -1,26 +1,2 @@
 require("allef.core")
 require("allef.lazy")
-
--- require("user.settings")
--- require("user.keymaps")
--- require("user.plugins")
--- require("user.general")
--- require("user.colorscheme")
-
--- require("user.lsp")
--- require("user.cmp")
--- require("user.tailwind")
-
--- require("user.treesitter")
--- require("user.telescope")
--- require("user.harpoon")
--- require("user.tree")
-
--- require("user.git")
--- require("user.nullls")
--- require("user.fix")
-
--- require("user.rest")
--- require("user.obsidian")
--- require("user.autopairs")
-
