15284386836eedd501a73db8831bd7415c96d660
Allef Douglas <allefwalker73@gmail.com>
2025-02-10 08:16:32 -0300

    fix gitsigns

--- lua/allef/plugins/gitsigns.lua
+++ lua/allef/plugins/gitsigns.lua
@@ -1,42 +1,42 @@
 return {
 	"lewis6991/gitsigns.nvim",
-	opts = {
-		signs = {
-			add = { text = "▎" },
-			change = { text = "▎" },
-			delete = { text = "契" },
-			topdelete = { text = "契" },
-			changedelete = { text = "▎" },
-		},
-		signcolumn = true,
-		numhl = false,
-		linehl = false,
-		word_diff = false,
-		watch_gitdir = {
-			interval = 1000,
-			follow_files = true,
-		},
-		attach_to_untracked = true,
-		current_line_blame = true,
-		current_line_blame_opts = {
-			virt_text = true,
-			virt_text_pos = "eol",
-			delay = 300,
-			ignore_whitespace = false,
-		},
-		sign_priority = 6,
-		update_debounce = 100,
-		status_formatter = nil,
-		max_file_length = 40000,
-		preview_config = {
-			border = "single",
-			style = "minimal",
-			relative = "cursor",
-			row = 0,
-			col = 1,
-		},
-	},
 	config = function()
+		require("gitsigns").setup({
+			signs = {
+				add = { text = "▎" },
+				change = { text = "▎" },
+				delete = { text = "契" },
+				topdelete = { text = "契" },
+				changedelete = { text = "▎" },
+			},
+			signcolumn = true,
+			numhl = false,
+			linehl = false,
+			word_diff = false,
+			watch_gitdir = {
+				interval = 1000,
+				follow_files = true,
+			},
+			attach_to_untracked = true,
+			current_line_blame = true,
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol",
+				delay = 300,
+				ignore_whitespace = false,
+			},
+			sign_priority = 6,
+			update_debounce = 100,
+			status_formatter = nil,
+			max_file_length = 40000,
+			preview_config = {
+				border = "single",
+				style = "minimal",
+				relative = "cursor",
+				row = 0,
+				col = 1,
+			},
+		})
 		vim.api.nvim_set_hl(0, "GitSignsAdd", { link = "DiffAdd" })
 		vim.api.nvim_set_hl(0, "GitSignsChange", { link = "DiffChange" })
 		vim.api.nvim_set_hl(0, "GitSignsDelete", { link = "DiffDelete" })
