6a90f9676692090b756465455d277ec7b200ea1a
Allef Douglas <allefwalker73@gmail.com>
2026-08-07 16:04:09 -0300

    fix tmux navigator: remove disable_when_zoomed que anulava o preserve_zoom

--- lua/allef/plugins/tmux-navigator.lua
+++ lua/allef/plugins/tmux-navigator.lua
@@ -4,8 +4,10 @@ return {
     lazy = false,
     init = function()
         vim.g.tmux_navigator_no_wrap = 1
+        -- preserve_zoom navega mantendo o zoom. disable_when_zoomed faria o
+        -- oposto (bloquear a navegação com o pane zoomed) -- os dois juntos se
+        -- anulam e matam o C-h/j/k/l sempre que o pane está zoomed (prefix+f).
         vim.g.tmux_navigator_preserve_zoom = 1
-        vim.g.tmux_navigator_disable_when_zoomed = 1
     end,
     config = function()
         -- Keymaps definidos após VimEnter para garantir que não sejam sobrescritos
